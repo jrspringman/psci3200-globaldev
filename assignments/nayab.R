cf778be6f53efe92d324d9f9bd90a3d6a41029d9
jrspringman <jeremyrgs6@gmail.com>
2024-04-26 17:50:49 -0400

    change PNG to png; weird diff in linux vs windows behavior

--- assignments/nayab.R
+++ assignments/nayab.R
@@ -1,6 +1,6 @@
 
 library(readstata13)
-
+library(dplyr)
 dat = readstata13::read.dta13("/home/jeremy/Downloads/bias_replication/justice_analysis.dta")
 
 
@@ -8,38 +8,38 @@ dat.new <- dat %>%
   group_by(state_code, year) %>%
   summarise(
     #generating acquittal counts and rates for females
-    total_cases_women_def = sum(def_name_female == 1),
-    female_def_acquitted = sum(def_name_female == 1 & disp_name == 4),
+    total_cases_women_def = sum(def_female == 1, na.rm = T),
+    female_def_acquitted = sum(def_female == 1 & disp_name == 4, na.rm = T),
     female_def_aquittal_rate = (female_def_acquitted/total_cases_women_def),
-    total_cases_women_pet = sum(pet_name_female ==1),
-    female_pet_acquitted = sum(pet_name_female == 1 & disp_name == 4),
+    total_cases_women_pet = sum(pet_female ==1, na.rm = T),
+    female_pet_acquitted = sum(pet_female == 1 & disp_name == 4, na.rm = T),
     female_pet_aquittal_rate = (female_pet_acquitted/total_cases_women_pet),
     #generating acquittal counts and rates for males
-    total_cases_male_def = sum(def_name_female == 0),
-    male_def_acquitted = sum(def_name_female == 0 & disp_name == 4),
+    total_cases_male_def = sum(def_female == 0, na.rm = T),
+    male_def_acquitted = sum(def_female == 0 & disp_name == 4, na.rm = T),
     male_def_aquittal_rate = (male_def_acquitted/total_cases_male_def),
-    total_cases_male_pet = sum(pet_name_female == 0),
-    male_pet_acquitted = sum(pet_name_female == 0 & disp_name == 4),
+    total_cases_male_pet = sum(pet_female == 0, na.rm = T),
+    male_pet_acquitted = sum(pet_female == 0 & disp_name == 4, na.rm = T),
     male_pet_aquittal_rate = (male_pet_acquitted/total_cases_male_pet),
     #generating dismissal counts and rates for females
-    female_def_dismissed = sum(def_name_female == 1 & disp_name == 22),
+    female_def_dismissed = sum(def_female == 1 & disp_name == 22, na.rm = T),
     female_def_dismissal_rate = (female_def_dismissed/total_cases_women_def),
-    female_pet_dismissed = sum(pet_name_female == 1 & disp_name == 22),
+    female_pet_dismissed = sum(pet_female == 1 & disp_name == 22, na.rm = T),
     female_pet_dismissal_rate = (female_pet_dismissed/total_cases_women_pet),
     #generating dismissal counts and rates for males
-    male_def_dismissed = sum(def_name_female == 0 & disp_name == 22),
+    male_def_dismissed = sum(def_female == 0 & disp_name == 22, na.rm = T),
     male_def_dismissal_rate = (male_def_dismissed/total_cases_male_def),
-    male_pet_dismissed = sum(pet_name_female == 0 & disp_name == 22),
+    male_pet_dismissed = sum(pet_female == 0 & disp_name == 22, na.rm = T),
     male_pet_dismissal_rate = (male_pet_dismissed/total_cases_male_pet),
     #generating conviction counts and rates for females
-    female_def_convicted= sum(def_name_female == 1 & disp_name == 19),
+    female_def_convicted= sum(def_female == 1 & disp_name == 19, na.rm = T),
     female_def_convicted_rate = (female_def_convicted/total_cases_women_def),
-    female_pet_convicted = sum(pet_name_female == 1 & disp_name == 19),
+    female_pet_convicted = sum(pet_female == 1 & disp_name == 19, na.rm = T),
     female_pet_convicted_rate = (female_pet_convicted/total_cases_women_pet),
     #generating conviction counts and rates for males
-    male_def_convicted = sum(def_name_female == 0 & disp_name == 19),
+    male_def_convicted = sum(def_female == 0 & disp_name == 19, na.rm = T),
     male_def_convicted_rate = (male_def_convicted/total_cases_male_def),
-    male_pet_convicted = sum(pet_name_female == 0 & disp_name == 19),
+    male_pet_convicted = sum(pet_female == 0 & disp_name == 19, na.rm = T),
     male_pet_convicted_rate = (male_pet_convicted/total_cases_male_pet))
 
-
+write.csv(dat.new, "/home/jeremy/Downloads/bias_replication/justice_analysis.csv")
